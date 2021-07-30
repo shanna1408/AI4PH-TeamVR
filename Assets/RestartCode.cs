675ee3458e7d69c3d015a0cea01e4983e579562f
irenemdaniel <86761772+irenemdaniel@users.noreply.github.com>
2021-07-29 20:27:14 -0600

    Add files via upload
    Sorry for the name, I really don't know how to change it without messing stuff up!! :)

---
+++ Assets/RestartCode.cs
@@ -0,0 +1,17 @@
+//using System.Collections;
+//using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Restart : MonoBehaviour
+{
+    public void Start()
+    {
+
+
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("SampleScene")
